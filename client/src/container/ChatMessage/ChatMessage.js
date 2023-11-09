09da99100afe6dc81628b908b0d04975bc0ea462
PhanVPhuc <pphuc1996@gmail.com>
2023-11-09 10:47:29 +0700

    update UI chatmessage

--- client/src/container/ChatMessage/ChatMessage.js
+++ client/src/container/ChatMessage/ChatMessage.js
@@ -6,7 +6,6 @@ function ChatMessage() {
             <div className="chatmessage">
                 <div className="form-chat">
                 <div className="col-12">
-                    {/* <h3 className="center">Chat Message</h3> */}
                     <div className="py-1 px-2 border-bottom d-none d-lg-block">
                     <div className="d-flex align-items-center py-1">
                         <div className="position-relative">
@@ -24,16 +23,13 @@ function ChatMessage() {
                     <div className="position-relative">
                         <div className="chat-messages p-1">
                             <div className="chat-message-right mw-65">
-                                <div className="flex-shrink-1 py-2 px-3 mr-3">ko co chi het ko co chi het ko co chi het ko co chi het ko co chi het</div>
+                                <div className="flex-shrink-1 py-2 px-3 mr-3">ko co chi</div>
                             </div>
                             <div className="d-flex chat-message-left mw-65">
                                 <img src={img}
                                     className="rounded-circle mr-1 avatar"
                                     width={40}
                                     height={40}/>
-                                {/* <div className="text-muted small text-nowrap mt-2">
-                                    2:34 am
-                                </div> */}
                                 <div className="flex-shrink-1 bg-#ccc py-2 px-3 ml-3">ok</div>
                             </div> 
                         </div>
